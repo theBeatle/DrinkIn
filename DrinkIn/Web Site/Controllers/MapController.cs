811f60ff80dd8ee2355fc6528016d8027685073c
Ivan Smyalko <post.mrfreddy@gmail.com>
2018-05-15 19:14:45 +0300

    Map Controller added

---
+++ DrinkIn/Web Site/Controllers/MapController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Site.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult Index()
+        {
+            ViewBag.Transparent = true;
+            return View();
+        }
+    }
+}
