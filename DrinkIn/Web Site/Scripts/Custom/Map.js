9d2b578a7a374c36388267265f96fdaf6675a03c
Ivan Smyalko <post.mrfreddy@gmail.com>
2018-05-18 14:20:00 +0300

    Setting scanner radius by Slider has been added

--- DrinkIn/Web Site/Scripts/Custom/Map.js
+++ DrinkIn/Web Site/Scripts/Custom/Map.js
@@ -2,6 +2,8 @@
 var marker;
 
 var RadarCircle;
+var currentRadius = 1000;
+var markerLocation;
 
 window.onload = function () {
     if (document.querySelectorAll('#myMap').length > 0) {
@@ -53,9 +55,12 @@ function initMap() {
 
 
 function placeMarker(location) {
+
+    markerLocation = location;
+
     if (!marker) {
         marker = new google.maps.Marker({
-            position: location,
+            position: markerLocation,
             draggable: true,
             animation: google.maps.Animation.DROP,
             map: mapElem
@@ -72,11 +77,44 @@ function placeMarker(location) {
             fillColor: '#ED6D1A',
             fillOpacity: 0.35,
             map: mapElem,
-            center: location,
-            radius: 1000
+            center: markerLocation,
+            radius: currentRadius
         });
         marker.setPosition(location);
     }
 }
 
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    currentRadius = this.value;
+    output.innerHTML = "Radius: " + currentRadius + "km";
+    RadarCircle.setRadius(currentRadius * 1000);
+    //RadarCircle.center = markerLocation;
+
+    //RadarCircle.setMap(mapElem);
+
+    //if (RadarCircle != null) {
+    //    RadarCircle.setMap(null);
+    //}
+
+    //RadarCircle = new google.maps.Circle({
+    //    strokeColor: '#ED6D1A',
+    //    strokeOpacity: 0.8,
+    //    strokeWeight: 2,
+    //    fillColor: '#ED6D1A',
+    //    fillOpacity: 0.35,
+    //    map: mapElem,
+    //    center: markerLocation,
+    //    radius: RadarCircle.radius  
+    //});
+
+
+
+
+    console.log(RadarCircle.radius);
+} 
